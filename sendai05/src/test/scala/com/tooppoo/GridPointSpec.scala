daff59c25dcd9eae4225f649a61d880c75d02590
philomagi <tooppoo56@gmail.com>
2020-08-03 04:29:30 +0900

    TODO追加

--- sendai05/src/test/scala/com/tooppoo/GridPointSpec.scala
+++ sendai05/src/test/scala/com/tooppoo/GridPointSpec.scala
@@ -2,6 +2,18 @@ package com.tooppoo
 
 import org.scalatest.funspec.AnyFunSpec
 
+/**
+ * [x] x座標とy座標を与えて格子点を生成してください
+ * [x] 生成した格子点からx座標とy座標を取得してください
+ * [x] 生成した格子点から文字列表記 (notation) を取得してください
+ * [ ] ２つの格子点が同じ座標(coordinates)を持つか判定してください
+ *   [ ] ==実装
+ *   [ ] x/y座標が一致→true
+ *   [ ] x座標だけ同じ→false
+ *   [ ] y座標だけ同じ→false
+ *   [ ] x/y座標両方違う→false
+ */
+
 class GridPointSpec extends AnyFunSpec {
   describe("文字列表記") {
     describe("x = 4, y = 7") {

461a901412426edcb8bb5d307482e39a31e3d61b
philomagi <tooppoo56@gmail.com>
2020-08-03 06:26:07 +0900

    ガードリファクタ

--- sendai05/src/main/scala/com/tooppoo/GridPointSet.scala
+++ sendai05/src/main/scala/com/tooppoo/GridPointSet.scala
@@ -3,13 +3,20 @@ package com.tooppoo
 class GridPointSet private (g1: GridPoint, g2: GridPoint, grids: GridPoint*) {
   private val allGrids: Seq[GridPoint] = Seq(g1, g2) concat grids
 
-  if (allGrids.groupBy(g => g.notation).size != allGrids.size) {
+  if (existDuplicateGrid) {
     throw new IllegalArgumentException("g1 and g2 must not same")
   }
 
   def contains(grid: GridPoint): Boolean = g1 == grid || g2 == grid
 
   def isConnected: Boolean = g1 isNeighborOf g2
+
+  private def existDuplicateGrid: Boolean = {
+    val groupedByNotation = allGrids.groupBy(g => g.notation)
+
+    // 重複が無ければ一致するはず
+    allGrids.size != groupedByNotation.size
+  }
 }
 
 object GridPointSet {

10bc3fc06f84e306cd07f3b4bba729f2e549a609
philomagi <tooppoo56@gmail.com>
2020-08-03 05:20:27 +0900

    ケース追加

--- sendai05/src/main/scala/com/tooppoo/GridPointSet.scala
+++ sendai05/src/main/scala/com/tooppoo/GridPointSet.scala
@@ -1,5 +1,5 @@
 package com.tooppoo
 
 case class GridPointSet(g1: GridPoint, g2: GridPoint) {
-
+  if (g1 == g2) throw new IllegalArgumentException("g1 and g2 must not same")
 }
--- sendai05/src/test/scala/com/tooppoo/GridPointSetSpec.scala
+++ sendai05/src/test/scala/com/tooppoo/GridPointSetSpec.scala
@@ -5,9 +5,9 @@ import org.scalatest.funspec.AnyFunSpec
 /**
  * [ ] 異なる座標を持つ２つの格子点を含む格子点集合(grid points)を導入します。
  *   [x] GridPointSet定義
- *   [ ] GridPointSetは２つの格子点を持つ
- *     [ ] 2つの格子点が異なる座標を持つ
- *     [ ] 2つの格子点が同じ座標を持つ
+ *   [x] GridPointSetは２つの格子点を持つ
+ *     [x] 2つの格子点が異なる座標を持つ
+ *     [x] 2つの格子点が同じ座標を持つ
  *   [ ] 格子点集合が、指定した格子点を含む(contains)かを判定してください
  *   [ ] 格子点集合が連結している(connected)かを判定してください
  *       (補足) 格子点集合に含まれる２つの格子点が隣り合っている場合に限り、その格子点集合が連結しているものとします
@@ -21,6 +21,16 @@ class GridPointSetSpec extends AnyFunSpec {
         val g2 = GridPoint(3, 6)
 
         GridPointSet(g1, g2)
+      }
+    }
+    describe("2つの格子点が同じ座標を持つ") {
+      it("生成できない") {
+        val g1 = GridPoint(4, 7)
+        val g2 = GridPoint(4, 7)
+
+        assertThrows[IllegalArgumentException] {
+          GridPointSet(g1, g2)
+        }
       }
     }
   }

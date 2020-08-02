b3d9329292999944ae9963176703cfc16aef98c2
philomagi <tooppoo56@gmail.com>
2020-08-03 04:26:47 +0900

    座標取得実装

--- sendai05/src/test/scala/com/tooppoo/GridPointSpec.scala
+++ sendai05/src/test/scala/com/tooppoo/GridPointSpec.scala
@@ -12,4 +12,20 @@ class GridPointSpec extends AnyFunSpec {
       }
     }
   }
+  describe("座標取得") {
+    describe("x = 4, y = 7") {
+      val p4_7 = GridPoint(4, 7)
+
+      describe("x座標") {
+        it("4") {
+          assert(p4_7.x == 4)
+        }
+      }
+      describe("y座標") {
+        it("7") {
+          assert(p4_7.y == 7)
+        }
+      }
+    }
+  }
 }

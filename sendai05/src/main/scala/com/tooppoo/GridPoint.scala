24a23f34d505bb8f4ea5d11d0585637f3ec8f0b6
philomagi <tooppoo56@gmail.com>
2020-08-04 03:17:24 +0900

    リネーム

--- sendai05/src/main/scala/com/tooppoo/GridPoint.scala
+++ sendai05/src/main/scala/com/tooppoo/GridPoint.scala
@@ -4,20 +4,20 @@ trait WithXY {
   val x: Int
   val y: Int
 
-  protected def isLeftOf(other: WithXY): Boolean = x - 1 == other.x && y == other.y
-  protected def isRightOf(other: WithXY): Boolean = x + 1 == other.x && y == other.y
-  protected def isUpOf(other: WithXY): Boolean = x == other.x && y - 1 == other.y
-  protected def isDownOf(other: WithXY): Boolean = x == other.x && y + 1 == other.y
+  protected def isWestOf(other: WithXY): Boolean = x - 1 == other.x && y == other.y
+  protected def isEastOf(other: WithXY): Boolean = x + 1 == other.x && y == other.y
+  protected def isNorthOf(other: WithXY): Boolean = x == other.x && y - 1 == other.y
+  protected def isSouthOf(other: WithXY): Boolean = x == other.x && y + 1 == other.y
 }
 
 case class TwoDimensionGridPoint(x: Int, y: Int) extends WithXY {
   def notation = s"($x,$y)"
 
   def isNeighborOf(other: TwoDimensionGridPoint): Boolean =
-    isLeftOf(other) ||
-    isRightOf(other) ||
-    isUpOf(other) ||
-    isDownOf(other)
+    isWestOf(other) ||
+    isEastOf(other) ||
+    isNorthOf(other) ||
+    isSouthOf(other)
 }
 
 object GridPoint {

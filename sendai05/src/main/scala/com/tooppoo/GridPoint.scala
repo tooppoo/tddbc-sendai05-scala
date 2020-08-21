3842140acd92283b635adbe46524397199df49cf
philomagi <tooppoo56@gmail.com>
2020-08-22 08:14:08 +0900

    メソッド分割

--- sendai05/src/main/scala/com/tooppoo/GridPoint.scala
+++ sendai05/src/main/scala/com/tooppoo/GridPoint.scala
@@ -9,10 +9,21 @@ case class TwoDimensionGridPoint(x: Int, y: Int) {
     isNorthOf(other) ||
     isSouthOf(other)
 
-  protected def isWestOf(other: TwoDimensionGridPoint): Boolean = x - 1 == other.x && y == other.y
-  protected def isEastOf(other: TwoDimensionGridPoint): Boolean = x + 1 == other.x && y == other.y
-  protected def isNorthOf(other: TwoDimensionGridPoint): Boolean = x == other.x && y - 1 == other.y
-  protected def isSouthOf(other: TwoDimensionGridPoint): Boolean = x == other.x && y + 1 == other.y
+  protected def isWestOf(other: TwoDimensionGridPoint): Boolean =
+    awayHorizontallyFrom(other, -1) && onSameHorizontalLineWith(other)
+  protected def isEastOf(other: TwoDimensionGridPoint): Boolean =
+    awayHorizontallyFrom(other, 1) && onSameHorizontalLineWith(other)
+
+  protected def isNorthOf(other: TwoDimensionGridPoint): Boolean =
+    onSameVerticalLineWith(other) && awayVerticallyFrom(other, -1)
+  protected def isSouthOf(other: TwoDimensionGridPoint): Boolean =
+    onSameVerticalLineWith(other) && awayVerticallyFrom(other, 1)
+
+  protected def awayHorizontallyFrom(point: TwoDimensionGridPoint, distance: Int): Boolean = point.x - x == distance
+  protected def awayVerticallyFrom(point: TwoDimensionGridPoint, distance: Int): Boolean = point.y - y == distance
+
+  protected def onSameHorizontalLineWith(other: TwoDimensionGridPoint): Boolean = y == other.y
+  protected def onSameVerticalLineWith(other: TwoDimensionGridPoint): Boolean = x == other.x
 }
 
 object GridPoint {

53765891575e7529044969419d32645f26d8b274
philomagi <tooppoo56@gmail.com>
2020-08-22 08:43:53 +0900

    directionオブジェクト導入

--- sendai05/src/main/scala/com/tooppoo/GridPoint.scala
+++ sendai05/src/main/scala/com/tooppoo/GridPoint.scala
@@ -10,20 +10,48 @@ case class TwoDimensionGridPoint(x: Int, y: Int) {
     isSouthOf(other)
 
   protected def isWestOf(other: TwoDimensionGridPoint): Boolean =
-    awayHorizontallyFrom(other, -1) && onSameHorizontalLineWith(other)
+    awayFrom(other, Direction.toWest(1)) && onSameHorizontalLineWith(other)
+
   protected def isEastOf(other: TwoDimensionGridPoint): Boolean =
-    awayHorizontallyFrom(other, 1) && onSameHorizontalLineWith(other)
+    awayFrom(other, Direction.toEast(1)) && onSameHorizontalLineWith(other)
 
   protected def isNorthOf(other: TwoDimensionGridPoint): Boolean =
-    onSameVerticalLineWith(other) && awayVerticallyFrom(other, -1)
+    onSameVerticalLineWith(other) && awayFrom(other, Direction.toNorth(1))
+
   protected def isSouthOf(other: TwoDimensionGridPoint): Boolean =
-    onSameVerticalLineWith(other) && awayVerticallyFrom(other, 1)
+    onSameVerticalLineWith(other) && awayFrom(other, Direction.toSouth(1))
 
-  protected def awayHorizontallyFrom(point: TwoDimensionGridPoint, distance: Int): Boolean = point.x - x == distance
-  protected def awayVerticallyFrom(point: TwoDimensionGridPoint, distance: Int): Boolean = point.y - y == distance
-
+  private def awayFrom(other: TwoDimensionGridPoint, direction: Direction): Boolean =
+    direction test (from = other, to = this)
   protected def onSameHorizontalLineWith(other: TwoDimensionGridPoint): Boolean = y == other.y
   protected def onSameVerticalLineWith(other: TwoDimensionGridPoint): Boolean = x == other.x
+
+  private trait Direction {
+    def test(from: TwoDimensionGridPoint, to: TwoDimensionGridPoint): Boolean
+  }
+  private object Direction {
+    final class West(private val distance: Int) extends Direction {
+      def test(from: TwoDimensionGridPoint, to: TwoDimensionGridPoint): Boolean =
+        from.x - distance == to.x
+    }
+    final class East(private val distance: Int) extends Direction {
+      def test(from: TwoDimensionGridPoint, to: TwoDimensionGridPoint): Boolean =
+        from.x + distance == to.x
+    }
+    final class North(private val distance: Int) extends Direction {
+      def test(from: TwoDimensionGridPoint, to: TwoDimensionGridPoint): Boolean =
+        from.y + distance == to.y
+    }
+    final class South(private val distance: Int) extends Direction {
+      def test(from: TwoDimensionGridPoint, to: TwoDimensionGridPoint): Boolean =
+        from.y - distance == to.y
+    }
+
+    def toWest(d: Int) = new West(d)
+    def toEast(d: Int) = new East(d)
+    def toNorth(d: Int) = new North(d)
+    def toSouth(d: Int) = new South(d)
+  }
 }
 
 object GridPoint {
